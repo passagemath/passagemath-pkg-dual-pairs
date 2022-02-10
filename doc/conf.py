e0fbe8d4d491f6cbcb7eca4a0678a3afb469c3fb
Peter Bruin <P.J.Bruin@math.leidenuniv.nl>
2022-02-10 23:21:19 +0100

    update year and version number

--- doc/conf.py
+++ doc/conf.py
@@ -24,13 +24,13 @@ except ImportError:
 # -- Project information -----------------------------------------------------
 
 project = u'Dual pairs of algebras'
-copyright = u'2019–2020, Peter Bruin'
+copyright = u'2019–2022, Peter Bruin'
 author = u'Peter Bruin'
 
 # The short X.Y version
-version = u'0.3'
+version = u'0.4'
 # The full version, including alpha/beta/rc tags
-release = u'0.3'
+release = u'0.4'
 
 
 # -- General configuration ---------------------------------------------------
