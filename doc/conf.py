fb65c14631727d1bc12b7b4b89f0b3346fc1e7ce
Peter Bruin <P.J.Bruin@math.leidenuniv.nl>
2022-02-21 20:03:18 +0100

    make sure Sphinx version >= 2 is installed

--- doc/conf.py
+++ doc/conf.py
@@ -36,8 +36,7 @@ release = u'0.4'
 # -- General configuration ---------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#
-needs_sphinx = '4.3.2'
+needs_sphinx = '2'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     url="https://gitlab.com/pbruin/dual-pairs",
     packages=["dual_pairs"],
     package_data={"dual_pairs": ["example_data/*.gp"]},
-    install_requires=["sagemath"],
+    install_requires=["sagemath", "sphinx>=2"],
     classifiers=[
         "Topic :: Scientific/Engineering :: Mathematics",
         "Intended Audience :: Science/Research",
