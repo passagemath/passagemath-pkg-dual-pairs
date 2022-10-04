9d3d8d9d2fdae2c4bcfacd2f5f0b99f2ba49fd8f
Peter Bruin <P.J.Bruin@math.leidenuniv.nl>
2022-10-04 14:31:03 +0200

    update version to 0.5

--- doc/conf.py
+++ doc/conf.py
@@ -28,9 +28,9 @@ copyright = u'2019–2022, Peter Bruin'
 author = u'Peter Bruin'
 
 # The short X.Y version
-version = u'0.4'
+version = u'0.5'
 # The full version, including alpha/beta/rc tags
-release = u'0.4'
+release = u'0.5'
 
 
 # -- General configuration ---------------------------------------------------
